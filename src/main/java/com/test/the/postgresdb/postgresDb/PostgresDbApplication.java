98d49b34290b83bfb90a5aacc930868e05a1de82
Franz Buse <franz.buse@tu-ilmenau.de>
2018-06-29 18:55:27 +0200

    first commit

---
+++ src/main/java/com/test/the/postgresdb/postgresDb/PostgresDbApplication.java
@@ -0,0 +1,12 @@
+package com.test.the.postgresdb.postgresDb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostgresDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostgresDbApplication.class, args);
+	}
+}
